8fdb4e9af38e3f2b01ee08169b40edecc778a88c
Joris Bertomeu <joris.bertomeu@epitech.eu>
2016-07-25 15:52:33 +0200

    board token checking + model checking for new board

--- baylibre/app/controllers/boards.js
+++ baylibre/app/controllers/boards.js
@@ -1,27 +1,42 @@
-var Board     = require('../models/board');
+var Board = require('../models/board');
+var Utils = require('../utils');
 
 exports._postL1 = function(req, res) {
     var board = new Board();
+    var missing;
 	
-    board.name = req.body.name;
-    board.manufacturer = req.body.manufacturer;
-    board.model = req.body.model;
-    board.image = req.body.mode;
+    Utils.checkToken(req, res, true).then(function(result) {
+	if (!result) {
+	    Utils.sendUnauthorized(req, res);
+	    return;
+	}
+	missing = Utils.checkFields(req.body, ["name", "manufacturer", "model", "image"]);
+	if (missing.length != 0)
+	    return res.send({'error': "Missing followwing properties : " + missing});
+	board.name = req.body.name;
+	board.manufacturer = req.body.manufacturer;
+	board.model = req.body.model;
+	board.image = req.body.mode;
 	    
-    board.save(function(err) {
-	if (err)
-	    res.send(err);
-
-	res.json({ message: 'Board created!' });
-    });	
+	board.save(function(err) {
+	    if (err)
+		res.send(err);
+	    res.json({ message: 'Board created!' });
+	});
+    });
 };
 
 exports._getL1 = function(req, res) {
-    Board.find(function(err, boards) {
-	if (err)
-	    res.send(err);
-	    
-	res.json(boards);
+    Utils.checkToken(req, res, true).then(function(result) {
+	if (!result) {
+	    Utils.sendUnauthorized(req, res);
+	    return;
+	}
+	Board.find(function(err, boards) {
+	    if (err)
+		res.send(err);
+	    res.json(boards);
+	});
     });
 };
 
@@ -34,28 +49,38 @@ exports._getL2 = function(req, res) {
 };
 
 exports._putL2 = function(req, res) {
-    Board.findById(req.params.board_id, function(err, board) { 
-	if (err)
-	    res.send(err);
-	    
-	board.name = req.body.name;
-	board.save(function(err) {
+    Utils.checkToken(req, res, true).then(function(result) {
+	if (!result) {
+	    Utils.sendUnauthorized(req, res);
+	    return;
+	}
+	Board.findById(req.params.board_id, function(err, board) { 
 	    if (err)
 		res.send(err);
-		
-	    res.json({ message: 'Board updated!' });
-	});	    
+	    
+	    board.name = req.body.name;
+	    board.save(function(err) {
+		if (err)
+		    res.send(err);
+		res.json({ message: 'Board updated!' });
+	    });	    
+	});
     });
 };
 
 exports._deleteL2 = function(req, res) {
-    Board.remove({
-	_id: req.params.board_id
-    }, function(err, board) {
-	if (err)
-	    res.send(err);
-	    
-	res.json({ message: 'Successfully deleted' });
+    Utils.checkToken(req, res, true).then(function(result) {
+	if (!result) {
+	    Utils.sendUnauthorized(req, res);
+	    return;
+	}
+	Board.remove({
+	    _id: req.params.board_id
+	}, function(err, board) {
+	    if (err)
+		res.send(err);	    
+	    res.json({ message: 'Successfully deleted' });
+	});
     });
 };
 
